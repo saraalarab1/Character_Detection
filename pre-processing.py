52b13d70d887febe1723b13bc917cc5d69566df1
Sara Al Arab <saraalarab2000@gmail.com>
2022-10-15 13:46:51 +0300

    fix symmetry and ratio (31.6 accuracy)

--- pre-processing.py
+++ pre-processing.py
@@ -27,7 +27,6 @@ def read_csv():
 def pre_process_images():
     images_dir = os.listdir('Img')
     for i in range(len(images_dir)):
-        print(i)
         image_name = images_dir[i]
         image_path = os.path.join('Img_2', image_name)
         image = cv.imread(image_path)
@@ -36,7 +35,6 @@ def pre_process_images():
         thresh_image = cv.threshold(gray_image, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
         contours = cv.findContours(thresh_image, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
         contours = contours[0] if len(contours) == 2 else contours[1]
-
         max_area = 0
         current_variables =  (0,0,0,0)
         dim = (WIDTH, HEIGHT)
@@ -54,13 +52,13 @@ def pre_process_images():
             # resize image
             image = cv.resize(image, dim, interpolation = cv.INTER_AREA)
             # Feature 2
-            training_dataset[image_name]["percentage_of_vertical_symmetry"] = get_vertical_symmetry_feature(image)
+            training_dataset[image_name]["vertical_ratio"] = get_vertical_ratio(image)
             # Feature 3
-            training_dataset[image_name]["percentage_of_horizontal_symmetry"] = get_horizontal_symmetry_feature(image)
+            training_dataset[image_name]["horizontal_ratio"] = get_horizontal_ratio(image)
             # Feature 4
-            training_dataset[image_name]["vertical_ratio"] = get_vertical_percentage_feature(image)
+            training_dataset[image_name]["percentage_of_vertical_symmetry"] = get_vertical_symmetry(image)
             # Feature 5
-            training_dataset[image_name]["horizontal_ratio"] = get_horizontal_percentage_feature(image)
+            training_dataset[image_name]["percentage_of_horizontal_symmetry"] = get_horizontal_symmetry(image)
             # Feature 6
             training_dataset[image_name]["percentage_of_pixels_at_horizontal_center"] = percentage_of_pixels_on_horizontal_center(image)
             # Feature 7 
@@ -73,44 +71,39 @@ def pre_process_images():
             training_dataset[image_name]["vertical_histogram_projection"] = get_vertical_histogram_projection(image)
             # Feature 11
             training_dataset[image_name]["horizontal_histogram_projection"] = get_horizontal_histogram_projection(image)
+            
+def get_aspect_ratio(image):
+    return  float(image.shape[1]) / image.shape[0]
 
+def get_vertical_ratio(image):
+    image_left = image[:,:int(WIDTH/2)]
+    image_right = image[:, int(WIDTH/2):]
+    total_pixel_left = np.sum(image_left == 0)
+    total_pixel_right =  np.sum(image_right == 0)
+    return total_pixel_left/total_pixel_right
 
-def get_vertical_symmetry_feature(image):
+def get_horizontal_ratio(image):
+    image_top = image[:int(HEIGHT/2), :]
+    image_bottom =  image[int(HEIGHT/2):, :]
+    total_pixel_top = np.sum(image_top == 0)
+    total_pixel_bottom = np.sum(image_bottom == 0)
+    return total_pixel_top/total_pixel_bottom
+
+def get_vertical_symmetry(image):
     image_left = image[:,:int(WIDTH/2)]
     image_right = cv.flip(image[:, int(WIDTH/2):],1)
     diff = cv.subtract(image_left, image_right)
-    err = np.sum(diff**2)
-    mse = err/(float(HEIGHT*WIDTH))
-    return mse
+    white_pixels = np.sum(diff == 255)
+    black_pixels = np.sum(diff == 0)
+    return white_pixels/(black_pixels+white_pixels)
 
-def get_horizontal_symmetry_feature(image):
-    image_top = image[: int(WIDTH/2), :]
-    image_bottom =  cv.flip(image[ int(WIDTH/2):, :],0)
+def get_horizontal_symmetry(image):
+    image_top = image[: int(HEIGHT/2), :]
+    image_bottom =  cv.flip(image[ int(HEIGHT/2):, :],0)
     diff = cv.subtract(image_top, image_bottom)
-    err = np.sum(diff**2)
-    mse = err/(float(HEIGHT*WIDTH))
-    return mse
-
-def get_vertical_percentage_feature(image):
-    image_left = image[:,:int(WIDTH/2)]
-    image_right = cv.flip(image[:, int(WIDTH/2):],1)
-    total_pixel_left = np.count_nonzero(np.all(image_left == [0,0,0], axis = 2))
-    total_pixel_right = np.count_nonzero(np.all(image_right == [0,0,0], axis = 2))
-    ratio_percentage = total_pixel_left/total_pixel_right
-    return ratio_percentage
-
-def get_horizontal_percentage_feature(image):
-    image_top = image[: int(WIDTH/2), :]
-    image_bottom =  cv.flip(image[ int(WIDTH/2):, :],0)
-    total_pixel_top = np.count_nonzero(np.all(image_top == [0,0,0], axis = 2))
-    total_pixel_bottom = np.count_nonzero(np.all(image_bottom == [0,0,0], axis = 2))
-    ratio_percentage = total_pixel_top/total_pixel_bottom
-    return ratio_percentage
-
-
-def get_aspect_ratio(image):
-    return  float(image.shape[1]) / image.shape[0]
-    
+    white_pixels = np.sum(diff == 255)
+    black_pixels = np.sum(diff == 0)
+    return white_pixels/(black_pixels+white_pixels)
 
 def get_horizontal_line_intersection(image):
     line = int(image.shape[0]/3)
@@ -262,8 +255,8 @@ def plot():
             index = index + 1
             if index == 1000:
                 break
-            zdata.append(data[i]["percentage_of_horizontal_symmetry"])
-            ydata.append(data[i]["percentage_of_pixels_at_vertical_center"])
+            zdata.append(data[i]["horizontal_ratio"])
+            ydata.append(data[i]["vertical_ratio"])
             xdata.append(data[i]["aspect_ratio"])
             colors.append(data[i]['color'])
             # plt.scatter(data[i]["feature_horizontal_ratio"], data[i]["feature_vertical_ratio"], c= data[i]["color"], s= 5)
@@ -272,8 +265,8 @@ def plot():
         ax.scatter3D(xdata, ydata, zdata, c=colors)
         plt.show()
 
-# read_csv()
-# pre_process_images()
-# create_json()
-# assign_random_colors()
+read_csv()
+pre_process_images()
+create_json()
+assign_random_colors()
 plot()
