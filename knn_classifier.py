cb7f802cb3cb59ece225ae2e12e1146ef7d2bf5a
Sara <sara.al-arab@logivations.com>
2022-10-19 18:38:28 +0300

    add knn model

--- knn_classifier.py
+++ knn_classifier.py
@@ -20,19 +20,18 @@ from sklearn.model_selection import LeavePOut #for P-cross validation
 from sklearn.metrics import classification_report, accuracy_score
 
 
-def train(X, y, k_cross_validation_ratio, testing_size, optimal_k=True, max_range_k=0 ):
+def train(X, Y, k_cross_validation_ratio, testing_size, optimal_k=True, max_range_k=0):
 
-    X0_train, X_test, y0_train, y_test = train_test_split(X,y,test_size=testing_size, random_state=7)
+    X0_train, X_test, Y0_train, Y_test = train_test_split(X,Y,test_size=testing_size, random_state=7)
     #Scaler is needed to scale all the inputs to a similar range
     # scaler = StandardScaler()
     # scaler = scaler.fit(X0_train)
     # X0_train = scaler.transform(X0_train)
     # X_test = scaler.transform(X_test)
-    #X_train, X_eval, y_train, y_eval = train_test_split(X0_train, y0_train, test_size= 100/k_cross_validation_ratio, random_state=7)
+    #X_train, X_eval, Y_train, y_eval = train_test_split(X0_train, Y0_train, test_size= 100/k_cross_validation_ratio, random_state=7)
     
 
-    #finding the range for the optimal value of k either within the specified range (user input) 
-    # or by our default range
+    # range for optimal K, can be specified by user
     if optimal_k and max_range_k>1:
         k_range= range(1, max_range_k)
     else:
@@ -46,17 +45,15 @@ def train(X, y, k_cross_validation_ratio, testing_size, optimal_k=True, max_rang
     print(k_range)
     for k in tqdm(k_range):
         knn = KNeighborsClassifier(n_neighbors=k)
-        knn.fit(X0_train, y0_train)
+        knn.fit(X0_train, Y0_train)
         y_pred = knn.predict(X_test)
-        scores[k] = metrics.accuracy_score(y_test, y_pred)
-        scores_list.append(metrics.accuracy_score(y_test, y_pred))
-    print(scores)
-    print(max((scores_list)))
-    k_optimal = scores_list.index(max(scores_list))
+        scores[k] = metrics.accuracy_score(Y_test, y_pred)
+        scores_list.append(round(metrics.accuracy_score(Y_test, y_pred),3))
+    print(scores_list)
+    k_optimal = scores_list.index(max(scores_list)) +1
     model = KNeighborsClassifier(n_neighbors= k_optimal)
     print(k_optimal)
-    return 
-
+     
     eval_score_list = []
     #Evaluation using cross validation: lpo: leave p out
     from sklearn.model_selection import StratifiedKFold
@@ -64,18 +61,18 @@ def train(X, y, k_cross_validation_ratio, testing_size, optimal_k=True, max_rang
     accuracys=[]
 
     skf = StratifiedKFold(n_splits=10, random_state=None)
-    skf.get_n_splits(X0_train, y0_train)
-    for train_index, test_index in skf.split(X0_train, y0_train):
+    skf.get_n_splits(X0_train, Y0_train)
+    for train_index, test_index in skf.split(X0_train, Y0_train):
     
         # print("TRAIN:", train_index, "Validation:", test_index)
         X_train, X_eval = pd.DataFrame(X0_train).iloc[train_index], pd.DataFrame(X0_train).iloc[test_index]
-        y_train, y_eval = pd.DataFrame(y0_train).iloc[train_index], pd.DataFrame(y0_train).iloc[test_index]
+        Y_train, y_eval = pd.DataFrame(Y0_train).iloc[train_index], pd.DataFrame(Y0_train).iloc[test_index]
     
-        model.fit(X0_train, y0_train)
+        model.fit(X0_train, Y0_train)
         predictions = model.predict(X_eval)
         score = accuracy_score(predictions, y_eval)
         accuracys.append(score)
-        #scores = cross_val_score(knn, X, y, cv=5, scoring='accuracy')
+        #scores = cross_val_score(knn, X, Y, cv=5, scoring='accuracy')
         #eval_score_list.append(scores.mean())
 
     #eval_accuracy = np.mean(eval_score_list)
@@ -85,24 +82,24 @@ def train(X, y, k_cross_validation_ratio, testing_size, optimal_k=True, max_rang
     model_name='pretrained_knn_model'
     pickle.dump(model, open(model_name, 'wb'))
 
-    return eval_accuracy, model, X0_train, y0_train, X_test, y_test
+    return eval_accuracy, model, X0_train, Y0_train, X_test, Y_test
 
 
-def test(X_train, y_train, X_test, y_test,pretrain_model=False):
-    model_name='pretrained_knn_model'
+def test(X_train, Y_train, X_test, Y_test,pretrain_model=False):
+
     if pretrain_model:
-        model = pickle.load(open(model_name, 'rb' ))
+        model = pickle.load(open('pretrained_knn_model', 'rb' ))
         
     else:
-        eval_score, model, X_train, y_train, X_test, y_test = train(X_test, y_test, pretrained_model=False)
+        eval_score, model, X_train, Y_train, X_test, Y_test = train(X_test, Y_test, pretrained_model=False)
         print("Evaluation score: {}".format(eval_score))
 
-    model.fit(X_train, y_train)
+    model.fit(X_train, Y_train)
     y_pred = model.predict(X_test)
-    print("Predictions shape: {}".format(y_pred.shape))
-    print("Y_test shape: {}".format(y_test))
-    classification_rep = classification_report(y_test, y_pred)
-    test_score = metrics.accuracy_score(y_test, y_pred)
+    print("Text Prediction: {}".format(y_pred.shape))
+    print("Y_test shape: {}".format(Y_test))
+    classification_rep = classification_report(Y_test, y_pred)
+    test_score = metrics.accuracy_score(Y_test, y_pred)
 
     return test_score, classification_rep
 
@@ -124,11 +121,11 @@ with open('data_with_colors.json', 'r') as f:
         arr_10 = data[i]['horizontal_line_intersection_count']
         arr_11 = data[i]['vertical_line_intersection_count']
 
-        x.append([arr_2[0],arr_2[1],arr_2[2],arr_2[3],arr_2[4],arr_2[5],arr_2[6],arr_2[7], arr_3[0], arr_3[1], arr_3[2],arr_3[3], arr_3[4], arr_3[5],arr_3[6], arr_3[7]])
+        x.append([arr_2[0],arr_2[1],arr_2[2],arr_2[3],arr_2[4],arr_2[5],arr_2[6],arr_2[7], arr_3[0], arr_3[1], arr_3[2],arr_3[3], arr_3[4], arr_3[5],arr_3[6],arr_3[7],])
         y.append(data[i]['label'])
 
-eval_accuracy, model, X_train, y_train, X_test, y_test = train(x, y, k_cross_validation_ratio=5, testing_size=0.2, max_range_k=100)
-# test_score, conf_rep = test(X_train, y_train,X_test, y_test, pretrain_model=True)
-# print("Evaluation Score: {}".format(eval_accuracy))
-# print("Test Score: {}".format(test_score))
-# print(conf_rep)
+eval_accuracy, model, X_train, Y_train, X_test, Y_test = train(x, y, k_cross_validation_ratio=5, testing_size=0.2, max_range_k=100)
+test_score, conf_rep = test(X_train, Y_train, X_test, Y_test, pretrain_model=True)
+print("Evaluation Score: {}".format(eval_accuracy))
+print("Test Score: {}".format(test_score))
+print(conf_rep)
