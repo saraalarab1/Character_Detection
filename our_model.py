a4fe14b5a8b04f71b79c02c39e8eb1633bd9ea68
Sara <sara.al-arab@logivations.com>
2022-11-07 01:20:50 +0200

    add our model

--- our_model.py
+++ our_model.py
@@ -1,4 +1,3 @@
-
 import json
 import numpy as np 
 import pickle
@@ -15,78 +14,7 @@ import matplotlib.pyplot as plt
 from mlxtend.plotting import plot_confusion_matrix
 
 printable = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-secondLayerLetters = 'UuvV'
-def train(x, y, testing_size, model_version, optimal_k=True, max_range_k=100):
-    
-    X0_train, X_test, Y0_train, Y_test = train_test_split(x,y,test_size=testing_size, random_state=7)
-    #Scaler is needed to scale all the inputs to a similar range
-    # scaler = StandardScaler()
-    # scaler = scaler.fit(X0_train)
-    # X0_train = scaler.transform(X0_train)
-    # X_test = scaler.transform(X_test)
-    #X_train, X_eval, Y_train, y_eval = train_test_split(X0_train, Y0_train, test_size= 100/k_cross_validation_ratio, random_state=7)
-
-    # range for optimal K, can be specified by user
-    if optimal_k and max_range_k>1:
-        k_range= range(1, max_range_k)
-    else:
-        k_range=range(1,50)
-    
-    scores = {}
-    scores_list = []
-
-    # Set the parameters by cross-validation
-
-    #finding the optimal nb of neighbors
-    for k in tqdm(k_range):
-        knn = KNeighborsClassifier(n_neighbors=k)
-        knn.fit(X0_train, Y0_train)
-        y_pred = knn.predict(X_test)
-        scores[k] = metrics.accuracy_score(Y_test, y_pred)
-        scores_list.append(round(metrics.accuracy_score(Y_test, y_pred),3))
-
-    k_optimal = scores_list.index(max(scores_list)) +1
-    print(f"K Optimal: {k_optimal}")
-    model = KNeighborsClassifier(n_neighbors= k_optimal)
-    model.fit(X0_train, Y0_train)
-     
-    accuracy = cross_val_score(model, X0_train, Y0_train, cv=5, scoring='accuracy')
-    print(f"{accuracy}")
-
-    accuracys=[]
-
-    #Evaluation using cross validation
-    # LeavePOut
-    # lpo = LeavePOut(p=2)
-    # KFold
-    # kf = KFold(n_splits=10)
-    # kf.get_n_splits(X0_train)
-    # StratifiedKFold
-    skf = StratifiedKFold(n_splits=10, random_state=None)
-    skf.get_n_splits(X0_train, Y0_train)
-    for train_index, test_index in skf.split(X0_train,Y0_train):
-    
-        # print("TRAIN:", train_index, "Validation:", test_index)
-        X_train, X_eval = df(X0_train).iloc[train_index], df(X0_train).iloc[test_index]
-        Y_train, y_eval = df(Y0_train).iloc[train_index], df(Y0_train).iloc[test_index]
-    
-        model.fit(X_train, Y_train.values.ravel())
-        predictions = model.predict(X_eval)
-        score = accuracy_score(predictions, y_eval)
-        accuracys.append(score)
-
-
-    eval_accuracy = np.mean(accuracys)
-
-    #save the pretrained model:
-    model_name='pretrained_knn_model.pkl'
-    if model_version:
-        pickle.dump(model, open(f"models/{model_version}/{model_name}", 'wb'))
-    else:
-        pickle.dump(model, open(f"models/knn/{model_name}", 'wb'))
-
-    return eval_accuracy, model, X_test, Y_test
-
+secondLayerLetters = '0OUVWXYZouvwxyz'
 
 def test(X_test, Y_test, model_version):
 
@@ -168,56 +96,55 @@ def train(features, labels = None):
         for i in data.keys():
             x.append(i)
             y.append(data[i]['label'])
-    X0_train, X_test, Y0_train, Y_test = train_test_split(x,y,test_size=0.3, random_state=7)
-    X0_train_features, _ = get_features(X0_train, Y0_train,  features)
-    knn = KNeighborsClassifier(n_neighbors=7)
-    knn.fit(X0_train_features, Y0_train)
-    secondLayer = pickle.load(open(f'models/svm/pretrained_svm_model.pkl', 'rb' ))
+    X0_train, X_test, Y0_train, Y_test = train_test_split(x,y,test_size=0.2, random_state=7)
+   
+    knn = pickle.load(open(f'models/ensemble/pretrained_ensemble_model.pkl', 'rb' ))
+    secondLayer = pickle.load(open(f'models/svm_case/pretrained_svm_model.pkl', 'rb' ))
 
     X_test_features, Y_test_features = get_features(X_test, Y_test, features)
     X_test_features2, Y_test_features2 = get_features(X_test, Y_test, features)
+
     Y_pred = knn.predict(X_test_features)
+    y_pred_proba = knn.predict_proba(X_test_features)
+
     cm = confusion_matrix(Y_pred, Y_test)
     # plot_confusion_matrix(knn, X_test_features, Y_test, cmap=plt.cm.Blues)
     plot_confusion_matrix(conf_mat=cm)
     plt.show()
-    y_pred_proba = knn.predict_proba(X_test_features)
+
     classification_rep = classification_report(Y_test, Y_pred,zero_division=True)
-    eval_accuracy = accuracy_score(Y_pred, Y_test)
-    print(f"Evaluation Before {eval_accuracy}")
+    score = metrics.accuracy_score(Y_test, Y_pred)
+    print("Before Score: ", score)
     # print(classification_rep)
+
     for i in range(len(y_pred_proba)):
         current_prediction_prob = max(y_pred_proba[i])
         if current_prediction_prob< 0.55:
             if Y_pred[i] in secondLayerLetters:
-                print(Y_pred[i])
-                # print(current_prediction_prob)
-                # print('possible labels')
                 possible_results = len([x for x in y_pred_proba[i] if x > 0]) # ['y, Y, q']
                 indices = sort_index(y_pred_proba[i])[:min(possible_results, 3)] # y 0.4 q 0.3 Y 0.3                 
                 labels = []
                 for index in indices:
                     labels.append(printable[index])
+                print('possible labels')
+                print(labels)
                 if labels[0] in secondLayerLetters:
                     new_labels = []
                     for label in labels: 
                         if label in secondLayerLetters:
-                            new_labels.append(label)  
+                            new_labels.append(label)  # y Y
+            
                 new_prediction = secondLayer.predict([X_test_features2[i]])[0]
-                cases_proba_max = max(secondLayer.predict_proba([X_test_features2[i]])[0])
-
-                if cases_proba_max > 0.65:
-                    if new_prediction == 'lower':
-                        print('new prediction: ' + Y_pred[i].lower())
-                    else:
-                        print('new prediction: ' + Y_pred[i].upper())
-                    print('old prediction: ' + Y_pred[i])
-                    print('correct prediction: ' + Y_test[i]) 
-                    Y_pred[i] = Y_pred[i].lower() if new_prediction == 'lower' else Y_pred[i].upper()
-                    print('-----------------------------------------')
-    eval_accuracy = accuracy_score(Y_pred, Y_test)
-    print(f"Evaluation AFter {eval_accuracy}")
+                print('new prediction: ' + new_prediction)
+                print('old prediction: ' + Y_pred[i])
+                print('correct prediction: ' + Y_test[i]) 
+                Y_pred[i] = new_prediction
+        # print('-----------------------------------------')
+        
     classification_rep = classification_report(Y_test, Y_pred,zero_division=True)
+    score = metrics.accuracy_score(Y_test, Y_pred)
+    print("After Score: ", score)
+    # print(classification_rep)
     am = confusion_matrix(Y_pred, Y_test)
     # plot_confusion_matrix(knn, X_test_features, Y_test, cmap=plt.cm.Blues)
     plot_confusion_matrix(conf_mat=am)
